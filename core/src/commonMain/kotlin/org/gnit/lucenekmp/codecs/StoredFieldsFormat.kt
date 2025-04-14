8f4a5b9efb1fbe932d902076ee1ba5f8fceb11f2
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-14 19:51:43 +0900

    StoredFieldsFormat compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/codecs/StoredFieldsFormat.kt
@@ -0,0 +1,25 @@
+package org.gnit.lucenekmp.codecs
+
+
+import kotlinx.io.IOException
+import org.gnit.lucenekmp.index.FieldInfos
+import org.gnit.lucenekmp.index.SegmentInfo
+import org.gnit.lucenekmp.store.Directory
+import org.gnit.lucenekmp.store.IOContext
+
+/** Controls the format of stored fields  */
+abstract class StoredFieldsFormat
+/** Sole constructor. (For invocation by subclass constructors, typically implicit.)  */
+protected constructor() {
+    /** Returns a [StoredFieldsReader] to load stored fields.  */
+    @Throws(IOException::class)
+    abstract fun fieldsReader(
+        directory: Directory, si: SegmentInfo, fn: FieldInfos, context: IOContext
+    ): StoredFieldsReader
+
+    /** Returns a [StoredFieldsWriter] to write stored fields.  */
+    @Throws(IOException::class)
+    abstract fun fieldsWriter(
+        directory: Directory, si: SegmentInfo, context: IOContext
+    ): StoredFieldsWriter
+}
