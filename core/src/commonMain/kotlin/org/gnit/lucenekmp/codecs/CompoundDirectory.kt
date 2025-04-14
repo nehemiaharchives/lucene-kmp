f5749718b348f8b1636bf66faa7bfe9e2c99b721
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-14 13:08:28 +0900

    CompoundDirectory compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/codecs/CompoundDirectory.kt
@@ -0,0 +1,67 @@
+package org.gnit.lucenekmp.codecs
+
+
+import kotlinx.io.IOException
+import org.gnit.lucenekmp.store.Directory
+import org.gnit.lucenekmp.store.IOContext
+import org.gnit.lucenekmp.store.IndexOutput
+import org.gnit.lucenekmp.store.Lock
+
+/**
+ * A read-only [Directory] that consists of a view over a compound file.
+ *
+ * @see CompoundFormat
+ *
+ * @lucene.experimental
+ */
+abstract class CompoundDirectory
+/** Sole constructor.  */
+protected constructor() : Directory() {
+    /**
+     * Checks consistency of this directory.
+     *
+     *
+     * Note that this may be costly in terms of I/O, e.g. may involve computing a checksum value
+     * against large data files.
+     */
+    @Throws(IOException::class)
+    abstract fun checkIntegrity()
+
+    /**
+     * Not implemented
+     *
+     * @throws UnsupportedOperationException always: not supported by CFS
+     */
+    override fun deleteFile(name: String) {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Not implemented
+     *
+     * @throws UnsupportedOperationException always: not supported by CFS
+     */
+    override fun rename(from: String, to: String) {
+        throw UnsupportedOperationException()
+    }
+
+    override fun syncMetaData() {}
+
+    @Throws(IOException::class)
+    override fun createOutput(name: String, context: IOContext): IndexOutput {
+        throw UnsupportedOperationException()
+    }
+
+    @Throws(IOException::class)
+    override fun createTempOutput(prefix: String, suffix: String, context: IOContext): IndexOutput {
+        throw UnsupportedOperationException()
+    }
+
+    override fun sync(names: MutableCollection<String>) {
+        throw UnsupportedOperationException()
+    }
+
+    override fun obtainLock(name: String): Lock {
+        throw UnsupportedOperationException()
+    }
+}
