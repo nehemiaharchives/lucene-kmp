d754ea2f5a83ec5d5465872578db5e246601ec55
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-14 20:48:54 +0900

    HnswGraphProvider compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/codecs/hnsw/HnswGraphProvider.kt
@@ -0,0 +1,22 @@
+package org.gnit.lucenekmp.codecs.hnsw
+
+import kotlinx.io.IOException
+import org.gnit.lucenekmp.util.hnsw.HnswGraph
+
+/**
+ * An interface that provides an HNSW graph. This interface is useful when gathering multiple HNSW
+ * graphs to bootstrap segment merging. The graph may be off the JVM heap.
+ *
+ * @lucene.experimental
+ */
+interface HnswGraphProvider {
+    /**
+     * Return the stored HnswGraph for the given field.
+     *
+     * @param field the field containing the graph
+     * @return the HnswGraph for the given field if found
+     * @throws IOException when reading potentially off-heap graph fails
+     */
+    @Throws(IOException::class)
+    fun getGraph(field: String): HnswGraph
+}
