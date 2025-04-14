b51d0c621518818a6d7f0e35b9be70d5e6fe9944
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-14 13:03:45 +0900

    MatchAllScorerSupplier compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/search/MatchAllScorerSupplier.kt
@@ -0,0 +1,31 @@
+package org.gnit.lucenekmp.search
+
+import kotlinx.io.IOException
+
+
+/**
+ * [ScorerSupplier] that matches all docs.
+ *
+ * @lucene.internal
+ */
+class MatchAllScorerSupplier
+/** Sole constructor  */(private val score: Float, private val scoreMode: ScoreMode, private val maxDoc: Int) :
+    ScorerSupplier() {
+    @Throws(IOException::class)
+    override fun get(leadCost: Long): Scorer {
+        return ConstantScoreScorer(score, scoreMode, DocIdSetIterator.all(maxDoc))
+    }
+
+    @Throws(IOException::class)
+    override fun bulkScorer(): BulkScorer {
+        if (maxDoc >= DenseConjunctionBulkScorer.WINDOW_SIZE / 2) {
+            return DenseConjunctionBulkScorer(mutableListOf(), maxDoc, score)
+        } else {
+            return super.bulkScorer()
+        }
+    }
+
+    override fun cost(): Long {
+        return maxDoc.toLong()
+    }
+}
