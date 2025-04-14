e241dffc85c3b2c77cfdbae28959c2a02221f95b
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-14 13:11:22 +0900

    FieldInfosFormat compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/codecs/FieldInfosFormat.kt
@@ -0,0 +1,33 @@
+package org.gnit.lucenekmp.codecs
+
+
+import kotlinx.io.IOException
+import org.gnit.lucenekmp.index.FieldInfos
+import org.gnit.lucenekmp.index.SegmentInfo
+import org.gnit.lucenekmp.store.Directory
+import org.gnit.lucenekmp.store.IOContext
+
+/**
+ * Encodes/decodes [FieldInfos]
+ *
+ * @lucene.experimental
+ */
+abstract class FieldInfosFormat
+/** Sole constructor. (For invocation by subclass constructors, typically implicit.)  */
+protected constructor() {
+    /** Read the [FieldInfos] previously written with [.write].  */
+    @Throws(IOException::class)
+    abstract fun read(
+        directory: Directory, segmentInfo: SegmentInfo, segmentSuffix: String, iocontext: IOContext
+    ): FieldInfos
+
+    /** Writes the provided [FieldInfos] to the directory.  */
+    @Throws(IOException::class)
+    abstract fun write(
+        directory: Directory,
+        segmentInfo: SegmentInfo,
+        segmentSuffix: String,
+        infos: FieldInfos,
+        context: IOContext
+    )
+}
