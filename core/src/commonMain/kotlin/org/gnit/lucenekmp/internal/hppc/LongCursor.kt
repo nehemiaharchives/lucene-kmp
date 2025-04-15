04115254a26606ba811c223792f42d86c228996e
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-15 15:36:26 +0900

    LongCursor compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/internal/hppc/LongCursor.kt
@@ -0,0 +1,22 @@
+package org.gnit.lucenekmp.internal.hppc
+
+
+/**
+ * Forked from HPPC, holding int index and long value.
+ *
+ * @lucene.internal
+ */
+class LongCursor {
+    /**
+     * The current value's index in the container this cursor belongs to. The meaning of this index is
+     * defined by the container (usually it will be an index in the underlying storage buffer).
+     */
+    var index: Int = 0
+
+    /** The current value.  */
+    var value: Long = 0
+
+    override fun toString(): String {
+        return "[cursor, index: $index, value: $value]"
+    }
+}
