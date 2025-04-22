a9e401bd8715009c05da7e9312aaff4dd645fae9
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-22 15:02:37 +0900

    RandomVectorScorerSupplier compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/util/hnsw/RandomVectorScorerSupplier.kt
@@ -0,0 +1,23 @@
+package org.gnit.lucenekmp.util.hnsw
+
+import kotlinx.io.IOException
+
+
+/** A supplier that creates [RandomVectorScorer] from an ordinal.  */
+interface RandomVectorScorerSupplier {
+    /**
+     * This creates a [UpdateableRandomVectorScorer] for scoring random nodes in batches against
+     * an ordinal.
+     *
+     * @return a new [UpdateableRandomVectorScorer]
+     */
+    @Throws(IOException::class)
+    fun scorer(): UpdateableRandomVectorScorer
+
+    /**
+     * Make a copy of the supplier, which will copy the underlying vectorValues so the copy is safe to
+     * be used in other threads.
+     */
+    @Throws(IOException::class)
+    fun copy(): RandomVectorScorerSupplier
+}
