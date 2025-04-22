9a33e8b2728ff0790663dbf8991595b6e9e77b07
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-22 20:06:43 +0900

    QuantizedVectorsReader compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/util/quantization/QuantizedVectorsReader.kt
@@ -0,0 +1,16 @@
+package org.gnit.lucenekmp.util.quantization
+
+import org.gnit.lucenekmp.util.Accountable
+import kotlinx.io.IOException
+
+/**
+ * Quantized vector reader
+ *
+ * @lucene.experimental
+ */
+interface QuantizedVectorsReader : AutoCloseable, Accountable {
+    @Throws(IOException::class)
+    fun getQuantizedVectorValues(fieldName: String): QuantizedByteVectorValues
+
+    fun getQuantizationState(fieldName: String): ScalarQuantizer
+}
