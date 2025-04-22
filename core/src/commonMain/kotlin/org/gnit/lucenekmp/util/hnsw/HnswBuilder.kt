23227be25a64f112a62155159f04527c77d009f2
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-22 17:30:26 +0900

    HnswBuilder compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/util/hnsw/HnswBuilder.kt
@@ -0,0 +1,31 @@
+package org.gnit.lucenekmp.util.hnsw
+
+import org.gnit.lucenekmp.util.InfoStream
+import kotlinx.io.IOException
+
+/**
+ * Interface for builder building the [OnHeapHnswGraph]
+ *
+ * @lucene.experimental
+ */
+interface HnswBuilder {
+    /**
+     * Adds all nodes to the graph up to the provided `maxOrd`.
+     *
+     * @param maxOrd The maximum ordinal (excluded) of the nodes to be added.
+     */
+    @Throws(IOException::class)
+    fun build(maxOrd: Int): OnHeapHnswGraph
+
+    /** Inserts a doc with vector value to the graph  */
+    @Throws(IOException::class)
+    fun addGraphNode(node: Int)
+
+    /** Set info-stream to output debugging information  */
+    fun setInfoStream(infoStream: InfoStream)
+
+    val graph: OnHeapHnswGraph
+
+    @get:Throws(IOException::class)
+    val completedGraph: OnHeapHnswGraph
+}
