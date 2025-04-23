df9d81625fa0cd9dd356c95404c6d45b4a3c293f
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-23 15:56:09 +0900

    InputStreamDataInput compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/store/InputStreamDataInput.kt
@@ -0,0 +1,47 @@
+package org.gnit.lucenekmp.store
+
+import kotlinx.io.EOFException
+import kotlinx.io.IOException
+import org.gnit.lucenekmp.jdkport.InputStream
+
+
+/** A [DataInput] wrapping a plain [InputStream].  */
+class InputStreamDataInput(private val `is`: InputStream) : DataInput(), AutoCloseable {
+
+    @Throws(IOException::class)
+    override fun readByte(): Byte {
+        val v: Int = `is`.read()
+        if (v == -1) throw EOFException()
+        return v.toByte()
+    }
+
+    @Throws(IOException::class)
+    override fun readBytes(b: ByteArray, offset: Int, len: Int) {
+        var offset = offset
+        var len = len
+        while (len > 0) {
+            val cnt: Int = `is`.read(b, offset, len)
+            if (cnt < 0) {
+                // Partially read the input, but no more data available in the stream.
+                throw EOFException()
+            }
+            len -= cnt
+            offset += cnt
+        }
+    }
+
+    @Throws(IOException::class)
+    override fun close() {
+        `is`.close()
+    }
+
+    @Throws(IOException::class)
+    override fun skipBytes(numBytes: Long) {
+        require(numBytes >= 0) { "numBytes must be >= 0, got $numBytes" }
+        val skipped: Long = `is`.skip(numBytes)
+        require(skipped <= numBytes)
+        if (skipped < numBytes) {
+            throw EOFException()
+        }
+    }
+}
