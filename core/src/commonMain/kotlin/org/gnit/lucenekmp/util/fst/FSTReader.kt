177b0d66ca4404b8939fb46fc0d930771ee5371a
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-23 17:18:42 +0900

    FSTReader compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/util/fst/FSTReader.kt
@@ -0,0 +1,25 @@
+package org.gnit.lucenekmp.util.fst
+
+import org.gnit.lucenekmp.util.fst.FST.BytesReader
+import org.gnit.lucenekmp.store.DataOutput
+import org.gnit.lucenekmp.util.Accountable
+import kotlinx.io.IOException
+
+/** Abstraction for reading bytes necessary for FST.  */
+interface FSTReader : Accountable {
+    /**
+     * Get the reverse BytesReader for this FST
+     *
+     * @return the reverse BytesReader
+     */
+    val reverseBytesReader: BytesReader
+
+    /**
+     * Write this FST to another DataOutput
+     *
+     * @param out the DataOutput
+     * @throws IOException if exception occurred during writing
+     */
+    @Throws(IOException::class)
+    fun writeTo(out: DataOutput)
+}
