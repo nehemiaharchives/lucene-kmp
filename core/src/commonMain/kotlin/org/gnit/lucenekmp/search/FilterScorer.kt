e0481387a16096717b488d858bc0bfe694450fe4
Hokuto Ide <nehemiaharchive@gmail.com>
2025-04-28 11:40:42 +0900

    FilterScorer compiles

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/search/FilterScorer.kt
@@ -0,0 +1,37 @@
+package org.gnit.lucenekmp.search
+
+import org.gnit.lucenekmp.util.Unwrappable
+import kotlinx.io.IOException
+
+/**
+ * A `FilterScorer` contains another `Scorer`, which it uses as its basic source of
+ * data, possibly transforming the data along the way or providing additional functionality. The
+ * class `FilterScorer` itself simply implements all abstract methods of `Scorer` with
+ * versions that pass all requests to the contained scorer. Subclasses of `FilterScorer` may
+ * further override some of these methods and may also provide additional methods and fields.
+ */
+abstract class FilterScorer(protected val `in`: Scorer) : Scorer(), Unwrappable<Scorer> {
+
+    @Throws(IOException::class)
+    override fun score(): Float {
+        return `in`.score()
+    }
+
+    // Leave maxScore abstract on purpose since the goal of this Filter class is
+    // to change the way the score is computed.
+    override fun docID(): Int {
+        return `in`.docID()
+    }
+
+    override fun iterator(): DocIdSetIterator {
+        return `in`.iterator()
+    }
+
+    override fun twoPhaseIterator(): TwoPhaseIterator {
+        return `in`.twoPhaseIterator()!!
+    }
+
+    override fun unwrap(): Scorer {
+        return `in`
+    }
+}
