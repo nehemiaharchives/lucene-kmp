7fd41223a1db49c60b193bba330e6cb718f917fe
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-14 06:38:43 +0900

    Add XYPolygon tests (#94)

--- test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/geo/ShapeTestUtil.kt
+++ test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/geo/ShapeTestUtil.kt
@@ -1,16 +1,25 @@
 package org.gnit.lucenekmp.tests.geo
 
+import org.gnit.lucenekmp.geo.XYPolygon
 import org.gnit.lucenekmp.geo.XYRectangle
 import org.gnit.lucenekmp.geo.XYLine
 import org.gnit.lucenekmp.tests.util.LuceneTestCase
 import org.gnit.lucenekmp.tests.util.TestUtil
 import kotlin.random.Random
 
+/** Utilities for generating random XY shapes for tests. */
 object ShapeTestUtil {
+  
+    // TODO which nextFloat implementation is good?
+    /*private fun nextFloat(random: Random): Float {
+        val sign = if (random.nextBoolean()) 1f else -1f
+        return random.nextFloat() * Float.MAX_VALUE * sign
+    }*/
+
     fun nextFloat(random: Random): Float {
         return ((random.nextDouble() * 2.0) - 1.0).toFloat() * Float.MAX_VALUE
     }
-
+  
     fun nextBox(random: Random): XYRectangle {
         var x0 = nextFloat(random)
         var x1 = nextFloat(random)
@@ -23,18 +32,37 @@ object ShapeTestUtil {
             y1 = nextFloat(random)
         }
         if (x1 < x0) {
-            val x = x0
+            val tmp = x0
             x0 = x1
-            x1 = x
+            x1 = tmp
         }
         if (y1 < y0) {
-            val y = y0
+            val tmp = y0
             y0 = y1
-            y1 = y
+            y1 = tmp
         }
         return XYRectangle(x0, x1, y0, y1)
     }
-    
+
+    private fun trianglePolygon(box: XYRectangle): XYPolygon {
+        val polyX = floatArrayOf(box.minX, box.maxX, box.maxX, box.minX)
+        val polyY = floatArrayOf(box.minY, box.minY, box.maxY, box.minY)
+        return XYPolygon(polyX, polyY)
+    }
+
+    private fun boxPolygon(box: XYRectangle): XYPolygon {
+        val polyX = floatArrayOf(box.minX, box.maxX, box.maxX, box.minX, box.minX)
+        val polyY = floatArrayOf(box.minY, box.minY, box.maxY, box.maxY, box.minY)
+        return XYPolygon(polyX, polyY)
+    }
+
+    /** Returns a simple random polygon for testing. */
+    fun nextPolygon(): XYPolygon {
+        val random = LuceneTestCase.random()
+        val box = nextBox(random)
+        return if (random.nextBoolean()) boxPolygon(box) else trianglePolygon(box)
+    }
+
     fun nextLine(): XYLine {
         val random = LuceneTestCase.random()
         val count = TestUtil.nextInt(random, 2, 10)
