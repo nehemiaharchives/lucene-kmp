e05189162e417e922393239f5e10874601ddcc63
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-14 06:06:57 +0900

    Add TestXYRectangle port and ShapeTestUtil (#97)

--- test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/geo/ShapeTestUtil.kt
+++ test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/geo/ShapeTestUtil.kt
@@ -1,15 +1,40 @@
 package org.gnit.lucenekmp.tests.geo
 
+import org.gnit.lucenekmp.geo.XYRectangle
 import org.gnit.lucenekmp.geo.XYLine
 import org.gnit.lucenekmp.tests.util.LuceneTestCase
 import org.gnit.lucenekmp.tests.util.TestUtil
 import kotlin.random.Random
 
 object ShapeTestUtil {
-    private fun nextFloat(random: Random): Float {
-        return (random.nextFloat() * 2 - 1) * Float.MAX_VALUE
+    fun nextFloat(random: Random): Float {
+        return ((random.nextDouble() * 2.0) - 1.0).toFloat() * Float.MAX_VALUE
     }
 
+    fun nextBox(random: Random): XYRectangle {
+        var x0 = nextFloat(random)
+        var x1 = nextFloat(random)
+        while (x0 == x1) {
+            x1 = nextFloat(random)
+        }
+        var y0 = nextFloat(random)
+        var y1 = nextFloat(random)
+        while (y0 == y1) {
+            y1 = nextFloat(random)
+        }
+        if (x1 < x0) {
+            val x = x0
+            x0 = x1
+            x1 = x
+        }
+        if (y1 < y0) {
+            val y = y0
+            y0 = y1
+            y1 = y
+        }
+        return XYRectangle(x0, x1, y0, y1)
+    }
+    
     fun nextLine(): XYLine {
         val random = LuceneTestCase.random()
         val count = TestUtil.nextInt(random, 2, 10)
