7a1fad7d15a2fbf64a254e1a6794af44d009fc48
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-14 21:18:07 +0900

    Port TestLine2D (#110)

--- test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/geo/GeoTestUtil.kt
+++ test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/geo/GeoTestUtil.kt
@@ -3,6 +3,8 @@ package org.gnit.lucenekmp.tests.geo
 import org.gnit.lucenekmp.tests.util.LuceneTestCase
 import org.gnit.lucenekmp.geo.Point
 
+import org.gnit.lucenekmp.geo.Line
+
 object GeoTestUtil {
     private const val MIN_LAT_INCL: Double = -90.0
     private const val MAX_LAT_INCL: Double = 90.0
@@ -15,6 +17,17 @@ object GeoTestUtil {
 
     fun nextLongitude(): Double {
         return MIN_LON_INCL + (MAX_LON_INCL - MIN_LON_INCL) * LuceneTestCase.random().nextDouble()
+    }
+
+    fun nextLine(): Line {
+        val size = LuceneTestCase.random().nextInt(2, 6)
+        val lats = DoubleArray(size)
+        val lons = DoubleArray(size)
+        for (i in 0 until size) {
+            lats[i] = nextLatitude()
+            lons[i] = nextLongitude()
+        }
+        return Line(lats, lons)
     }
 
     fun nextPoint(): Point {
