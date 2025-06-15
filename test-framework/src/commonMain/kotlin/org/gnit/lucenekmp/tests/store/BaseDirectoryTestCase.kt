64b395e63af4646f3de219ca365c6b7bec90da0f
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-15 20:45:16 +0900

    Port additional BaseDirectoryTestCase tests

--- test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/store/BaseDirectoryTestCase.kt
+++ test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/store/BaseDirectoryTestCase.kt
@@ -3,8 +3,10 @@ package org.gnit.lucenekmp.tests.store
 import okio.FileSystem
 import okio.Path
 import okio.Path.Companion.toPath
+import okio.EOFException
 import org.gnit.lucenekmp.store.Directory
 import org.gnit.lucenekmp.tests.util.LuceneTestCase
+import org.gnit.lucenekmp.tests.util.TestUtil
 import org.gnit.lucenekmp.store.IOContext
 import kotlin.random.Random
 
@@ -120,6 +122,190 @@ abstract class BaseDirectoryTestCase : LuceneTestCase() {
             dir.openInput("long", IOContext.DEFAULT).use { input ->
                 kotlin.test.assertEquals(8L, input.length())
                 kotlin.test.assertEquals(-5000L, input.readLong())
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testAlignedLittleEndianLongs() {
+        newDirectory().use { dir ->
+            dir.createOutput("littleEndianLongs", IOContext.DEFAULT).use { out ->
+                out.writeLong(3L)
+                out.writeLong(Long.MAX_VALUE)
+                out.writeLong(-3L)
+            }
+            dir.openInput("littleEndianLongs", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals(24L, input.length())
+                val l = LongArray(4)
+                input.readLongs(l, 1, 3)
+                kotlin.test.assertContentEquals(longArrayOf(0L, 3L, Long.MAX_VALUE, -3L), l)
+                kotlin.test.assertEquals(24L, input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testUnalignedLittleEndianLongs() {
+        newDirectory().use { dir ->
+            dir.createOutput("littleEndianLongs", IOContext.DEFAULT).use { out ->
+                out.writeByte(2)
+                out.writeLong(3L)
+                out.writeLong(Long.MAX_VALUE)
+                out.writeLong(-3L)
+            }
+            dir.openInput("littleEndianLongs", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals(25L, input.length())
+                kotlin.test.assertEquals(2, input.readByte().toInt())
+                val l = LongArray(4)
+                input.readLongs(l, 1, 3)
+                kotlin.test.assertContentEquals(longArrayOf(0L, 3L, Long.MAX_VALUE, -3L), l)
+                kotlin.test.assertEquals(25L, input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testLittleEndianLongsUnderflow() {
+        newDirectory().use { dir ->
+            val offset = Random.nextInt(8)
+            val length = TestUtil.nextInt(Random, 1, 16)
+            dir.createOutput("littleEndianLongs", IOContext.DEFAULT).use { out ->
+                val b = ByteArray(offset + length * Long.SIZE_BYTES - TestUtil.nextInt(Random, 1, Long.SIZE_BYTES))
+                Random.nextBytes(b)
+                out.writeBytes(b, b.size)
+            }
+            dir.openInput("littleEndianLongs", IOContext.DEFAULT).use { input ->
+                input.seek(offset.toLong())
+                LuceneTestCase.expectThrows(EOFException::class) {
+                    input.readLongs(LongArray(length), 0, length)
+                }
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testAlignedInts() {
+        newDirectory().use { dir ->
+            dir.createOutput("Ints", IOContext.DEFAULT).use { out ->
+                out.writeInt(3)
+                out.writeInt(Int.MAX_VALUE)
+                out.writeInt(-3)
+            }
+            dir.openInput("Ints", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals(12L, input.length())
+                val i = IntArray(4)
+                input.readInts(i, 1, 3)
+                kotlin.test.assertContentEquals(intArrayOf(0, 3, Int.MAX_VALUE, -3), i)
+                kotlin.test.assertEquals(12L, input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testUnalignedInts() {
+        val padding = Random.nextInt(3) + 1
+        newDirectory().use { dir ->
+            dir.createOutput("Ints", IOContext.DEFAULT).use { out ->
+                repeat(padding) { out.writeByte(2) }
+                out.writeInt(3)
+                out.writeInt(Int.MAX_VALUE)
+                out.writeInt(-3)
+            }
+            dir.openInput("Ints", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals((12 + padding).toLong(), input.length())
+                repeat(padding) { kotlin.test.assertEquals(2, input.readByte().toInt()) }
+                val i = IntArray(4)
+                input.readInts(i, 1, 3)
+                kotlin.test.assertContentEquals(intArrayOf(0, 3, Int.MAX_VALUE, -3), i)
+                kotlin.test.assertEquals((12 + padding).toLong(), input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testIntsUnderflow() {
+        newDirectory().use { dir ->
+            val offset = Random.nextInt(4)
+            val length = TestUtil.nextInt(Random, 1, 16)
+            dir.createOutput("Ints", IOContext.DEFAULT).use { out ->
+                val b = ByteArray(offset + length * Int.SIZE_BYTES - TestUtil.nextInt(Random, 1, Int.SIZE_BYTES))
+                Random.nextBytes(b)
+                out.writeBytes(b, b.size)
+            }
+            dir.openInput("Ints", IOContext.DEFAULT).use { input ->
+                input.seek(offset.toLong())
+                LuceneTestCase.expectThrows(EOFException::class) {
+                    input.readInts(IntArray(length), 0, length)
+                }
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testAlignedFloats() {
+        newDirectory().use { dir ->
+            dir.createOutput("Floats", IOContext.DEFAULT).use { out ->
+                out.writeInt(3f.toBits())
+                out.writeInt(Float.MAX_VALUE.toBits())
+                out.writeInt((-3f).toBits())
+            }
+            dir.openInput("Floats", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals(12L, input.length())
+                val ff = FloatArray(4)
+                input.readFloats(ff, 1, 3)
+                kotlin.test.assertContentEquals(floatArrayOf(0f, 3f, Float.MAX_VALUE, -3f), ff)
+                kotlin.test.assertEquals(12L, input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testUnalignedFloats() {
+        val padding = Random.nextInt(3) + 1
+        newDirectory().use { dir ->
+            dir.createOutput("Floats", IOContext.DEFAULT).use { out ->
+                repeat(padding) { out.writeByte(2) }
+                out.writeInt(3f.toBits())
+                out.writeInt(Float.MAX_VALUE.toBits())
+                out.writeInt((-3f).toBits())
+            }
+            dir.openInput("Floats", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals((12 + padding).toLong(), input.length())
+                repeat(padding) { kotlin.test.assertEquals(2, input.readByte().toInt()) }
+                val ff = FloatArray(4)
+                input.readFloats(ff, 1, 3)
+                kotlin.test.assertContentEquals(floatArrayOf(0f, 3f, Float.MAX_VALUE, -3f), ff)
+                kotlin.test.assertEquals((12 + padding).toLong(), input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testFloatsUnderflow() {
+        newDirectory().use { dir ->
+            val offset = Random.nextInt(4)
+            val length = TestUtil.nextInt(Random, 1, 16)
+            dir.createOutput("Floats", IOContext.DEFAULT).use { out ->
+                val b = ByteArray(offset + length * Float.SIZE_BYTES - TestUtil.nextInt(Random, 1, Float.SIZE_BYTES))
+                Random.nextBytes(b)
+                out.writeBytes(b, b.size)
+            }
+            dir.openInput("Floats", IOContext.DEFAULT).use { input ->
+                input.seek(offset.toLong())
+                LuceneTestCase.expectThrows(EOFException::class) {
+                    input.readFloats(FloatArray(length), 0, length)
+                }
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testString() {
+        newDirectory().use { dir ->
+            dir.createOutput("string", IOContext.DEFAULT).use { it.writeString("hello!") }
+            dir.openInput("string", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals("hello!", input.readString())
+                kotlin.test.assertEquals(7L, input.length())
             }
         }
     }
