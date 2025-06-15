97b93dad0ef0b398290d0be577e9a0290fa4a08c
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-15 17:24:23 +0900

    Add skeleton BaseChunkedDirectoryTestCase

---
+++ test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/store/BaseChunkedDirectoryTestCase.kt
@@ -0,0 +1,95 @@
+package org.gnit.lucenekmp.tests.store
+
+import okio.Path
+import org.gnit.lucenekmp.store.Directory
+import org.gnit.lucenekmp.tests.util.LuceneTestCase
+import org.gnit.lucenekmp.tests.util.TestUtil
+import kotlin.random.Random
+
+/**
+ * Ported from Lucene's BaseChunkedDirectoryTestCase.
+ * This is currently a skeleton to satisfy compilation.
+ */
+abstract class BaseChunkedDirectoryTestCase : LuceneTestCase() {
+    /** Returns a new directory instance for the given path and chunk size. */
+    @Throws(Exception::class)
+    protected abstract fun getDirectory(path: Path, maxChunkSize: Int): Directory
+
+    /** Returns a directory with a random chunk size. */
+    @Throws(Exception::class)
+    open fun getDirectory(path: Path): Directory {
+        val chunk = 1 shl TestUtil.nextInt(random(), 10, 20)
+        return getDirectory(path, chunk)
+    }
+
+    // The following test methods are placeholders for now.
+    @Throws(Exception::class)
+    open fun testGroupVIntMultiBlocks() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testCloneClose() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testCloneSliceClose() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSeekZero() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSeekSliceZero() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSeekEnd() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSeekSliceEnd() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSeeking() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSlicedSeeking() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testSliceOfSlice() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testRandomChunkSizes() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testBytesCrossBoundary() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testLittleEndianLongsCrossBoundary() {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(Exception::class)
+    open fun testLittleEndianFloatsCrossBoundary() {
+        TODO("Not yet implemented")
+    }
+}
