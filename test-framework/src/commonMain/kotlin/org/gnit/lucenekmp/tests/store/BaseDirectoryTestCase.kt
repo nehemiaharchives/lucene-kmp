9c72e38b1c3967cf49d532ee455df9140a765a8b
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-16 12:31:17 +0900

    Port additional BaseDirectoryTestCase tests

--- test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/store/BaseDirectoryTestCase.kt
+++ test-framework/src/commonMain/kotlin/org/gnit/lucenekmp/tests/store/BaseDirectoryTestCase.kt
@@ -4,6 +4,7 @@ import okio.FileSystem
 import okio.Path
 import okio.Path.Companion.toPath
 import okio.EOFException
+import okio.IOException
 import org.gnit.lucenekmp.store.Directory
 import org.gnit.lucenekmp.tests.util.LuceneTestCase
 import org.gnit.lucenekmp.tests.util.TestUtil
@@ -307,6 +308,179 @@ abstract class BaseDirectoryTestCase : LuceneTestCase() {
                 kotlin.test.assertEquals("hello!", input.readString())
                 kotlin.test.assertEquals(7L, input.length())
             }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testVInt() {
+        newDirectory().use { dir ->
+            dir.createOutput("vint", IOContext.DEFAULT).use { it.writeVInt(500) }
+            dir.openInput("vint", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals(2L, input.length())
+                kotlin.test.assertEquals(500, input.readVInt())
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testVLong() {
+        newDirectory().use { dir ->
+            dir.createOutput("vlong", IOContext.DEFAULT).use { it.writeVLong(Long.MAX_VALUE) }
+            dir.openInput("vlong", IOContext.DEFAULT).use { input ->
+                kotlin.test.assertEquals(9L, input.length())
+                kotlin.test.assertEquals(Long.MAX_VALUE, input.readVLong())
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testZInt() {
+        val ints = IntArray(Random.nextInt(10))
+        for (i in ints.indices) {
+            ints[i] = when (Random.nextInt(3)) {
+                0 -> Random.nextInt()
+                1 -> if (Random.nextBoolean()) Int.MIN_VALUE else Int.MAX_VALUE
+                else -> (if (Random.nextBoolean()) -1 else 1) * Random.nextInt(1024)
+            }
+        }
+
+        newDirectory().use { dir ->
+            dir.createOutput("zint", IOContext.DEFAULT).use { out ->
+                for (v in ints) out.writeZInt(v)
+            }
+            dir.openInput("zint", IOContext.DEFAULT).use { input ->
+                for (v in ints) kotlin.test.assertEquals(v, input.readZInt())
+                kotlin.test.assertEquals(input.length(), input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testZLong() {
+        val longs = LongArray(Random.nextInt(10))
+        for (i in longs.indices) {
+            longs[i] = when (Random.nextInt(3)) {
+                0 -> Random.nextLong()
+                1 -> if (Random.nextBoolean()) Long.MIN_VALUE else Long.MAX_VALUE
+                else -> (if (Random.nextBoolean()) -1 else 1) * Random.nextInt(1024).toLong()
+            }
+        }
+
+        newDirectory().use { dir ->
+            dir.createOutput("zlong", IOContext.DEFAULT).use { out ->
+                for (v in longs) out.writeZLong(v)
+            }
+            dir.openInput("zlong", IOContext.DEFAULT).use { input ->
+                for (v in longs) kotlin.test.assertEquals(v, input.readZLong())
+                kotlin.test.assertEquals(input.length(), input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testSetOfStrings() {
+        newDirectory().use { dir ->
+            dir.createOutput("stringset", IOContext.DEFAULT).use { out ->
+                out.writeSetOfStrings(mutableSetOf("test1", "test2"))
+                out.writeSetOfStrings(mutableSetOf())
+                out.writeSetOfStrings(mutableSetOf("test3"))
+            }
+            dir.openInput("stringset", IOContext.DEFAULT).use { input ->
+                val set1 = input.readSetOfStrings()
+                kotlin.test.assertEquals(setOf("test1", "test2"), set1)
+                set1.add("bogus")
+
+                val set2 = input.readSetOfStrings()
+                kotlin.test.assertTrue(set2.isEmpty())
+                set2.add("bogus")
+
+                val set3 = input.readSetOfStrings()
+                kotlin.test.assertEquals(setOf("test3"), set3)
+                set3.add("bogus")
+
+                kotlin.test.assertEquals(input.length(), input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testMapOfStrings() {
+        val m = mutableMapOf("test1" to "value1", "test2" to "value2")
+
+        newDirectory().use { dir ->
+            dir.createOutput("stringmap", IOContext.DEFAULT).use { out ->
+                out.writeMapOfStrings(m.toMutableMap())
+                out.writeMapOfStrings(mutableMapOf())
+                out.writeMapOfStrings(mutableMapOf("key" to "value"))
+            }
+            dir.openInput("stringmap", IOContext.DEFAULT).use { input ->
+                val map1 = input.readMapOfStrings()
+                kotlin.test.assertEquals(m, map1)
+                map1["bogus1"] = "bogus2"
+
+                val map2 = input.readMapOfStrings()
+                kotlin.test.assertTrue(map2.isEmpty())
+                map2["bogus1"] = "bogus2"
+
+                val map3 = input.readMapOfStrings()
+                kotlin.test.assertEquals(mapOf("key" to "value"), map3)
+                map3["bogus1"] = "bogus2"
+
+                kotlin.test.assertEquals(input.length(), input.filePointer)
+            }
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testChecksum() {
+        val expected = org.gnit.lucenekmp.jdkport.CRC32()
+        val numBytes = Random.nextInt(20000)
+        val bytes = ByteArray(numBytes).also { Random.nextBytes(it) }
+        expected.update(bytes, 0, bytes.size)
+
+        newDirectory().use { dir ->
+            dir.createOutput("checksum", IOContext.DEFAULT).use { it.writeBytes(bytes, 0, bytes.size) }
+
+            dir.openChecksumInput("checksum").use { input ->
+                input.skipBytes(numBytes.toLong())
+                kotlin.test.assertEquals(expected.getValue(), input.checksum)
+            }
+        }
+    }
+
+    @Throws(Throwable::class)
+    fun testDetectClose() {
+        val dir = newDirectory()
+        dir.close()
+        LuceneTestCase.expectThrows(org.gnit.lucenekmp.store.AlreadyClosedException::class) {
+            dir.createOutput("test", IOContext.DEFAULT)
+        }
+    }
+
+    @Throws(Exception::class)
+    fun testThreadSafetyInListAll() {
+        newDirectory().use { dir ->
+            val max = TestUtil.nextInt(Random, 500, 1000)
+            for (i in 0 until max) {
+                val fileName = "file-" + i
+                dir.createOutput(fileName, IOContext.DEFAULT).use { _ -> }
+                kotlin.test.assertTrue(slowFileExists(dir, fileName))
+                val files = dir.listAll()
+                if (files.isNotEmpty()) {
+                    val file = files[Random.nextInt(files.size)]
+                    dir.openInput(file, IOContext.DEFAULT).use { _ -> }
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    fun testFileExistsInListAfterCreated() {
+        newDirectory().use { dir ->
+            val name = "file"
+            dir.createOutput(name, IOContext.DEFAULT).close()
+            kotlin.test.assertTrue(slowFileExists(dir, name))
+            kotlin.test.assertTrue(dir.listAll().contains(name))
         }
     }
 
