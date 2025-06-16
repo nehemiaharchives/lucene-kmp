d65bc676554a755e6640d6ab4a57e9f2b5449698
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-16 18:41:11 +0900

    Port additional IntObjectHashMap tests

--- core/src/commonTest/kotlin/org/gnit/lucenekmp/internal/hppc/TestIntObjectHashMap.kt
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/internal/hppc/TestIntObjectHashMap.kt
@@ -5,7 +5,9 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import kotlin.test.assertFalse
 import kotlin.test.assertNull
+import kotlin.test.assertFailsWith
 
 class TestIntObjectHashMap : LuceneTestCase() {
     private lateinit var map: IntObjectHashMap<Any?>
@@ -59,6 +61,61 @@ class TestIntObjectHashMap : LuceneTestCase() {
             val cursor = c!!
             assertTrue(map.indexExists(cursor.index))
             assertEquals(cursor.value, map.indexGet(cursor.index))
+        }
+    }
+
+    @Test
+    fun testIndexMethods() {
+        map.put(0, 1)
+        map.put(1, 2)
+
+        assertTrue(map.indexOf(0) >= 0)
+        assertTrue(map.indexOf(1) >= 0)
+        assertTrue(map.indexOf(2) < 0)
+
+        assertTrue(map.indexExists(map.indexOf(0)))
+        assertTrue(map.indexExists(map.indexOf(1)))
+        assertFalse(map.indexExists(map.indexOf(2)))
+
+        assertEquals(1, map.indexGet(map.indexOf(0)))
+        assertEquals(2, map.indexGet(map.indexOf(1)))
+
+        assertFailsWith<IllegalArgumentException> { map.indexGet(map.indexOf(2)) }
+
+        assertEquals(1, map.indexReplace(map.indexOf(0), 3))
+        assertEquals(2, map.indexReplace(map.indexOf(1), 4))
+        assertEquals(3, map.indexGet(map.indexOf(0)))
+        assertEquals(4, map.indexGet(map.indexOf(1)))
+
+        map.indexInsert(map.indexOf(2), 2, 1)
+        assertEquals(1, map.indexGet(map.indexOf(2)))
+        assertEquals(3, map.size())
+
+        assertEquals(3, map.indexRemove(map.indexOf(0)))
+        assertEquals(2, map.size())
+        assertEquals(1, map.indexRemove(map.indexOf(2)))
+        assertEquals(1, map.size())
+        assertTrue(map.indexOf(0) < 0)
+        assertTrue(map.indexOf(1) >= 0)
+        assertTrue(map.indexOf(2) < 0)
+    }
+
+    @Test
+    fun testCloningConstructor() {
+        map.put(1, 1)
+        map.put(2, 2)
+        map.put(3, 3)
+
+        val clone = IntObjectHashMap<Any?>(map)
+        assertSameMap(map, clone)
+    }
+
+    private fun assertSameMap(c1: IntObjectHashMap<Any?>, c2: IntObjectHashMap<Any?>) {
+        assertEquals(c1.size(), c2.size())
+        for (entry in c1) {
+            val e = entry!!
+            assertTrue(c2.containsKey(e.key))
+            assertEquals(e.value, c2.get(e.key))
         }
     }
 
