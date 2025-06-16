4cc69616cd95f12418445c06edbe713bc959d885
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-17 08:54:00 +0900

    Fix bucket mapping for -1 in radix sorter

--- core/src/commonMain/kotlin/org/gnit/lucenekmp/util/MSBRadixSorter.kt
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/util/MSBRadixSorter.kt
@@ -167,7 +167,8 @@ abstract class MSBRadixSorter protected constructor(protected val maxLength: Int
 
     /** Return a number for the k-th character between 0 and [.HISTOGRAM_SIZE].  */
     protected fun getBucket(i: Int, k: Int): Int {
-        return (byteAt(i, k).toInt() and 0xff) + 1
+        val b = byteAt(i, k).toInt()
+        return if (b == -1) 0 else (b and 0xff) + 1
     }
 
     /**
