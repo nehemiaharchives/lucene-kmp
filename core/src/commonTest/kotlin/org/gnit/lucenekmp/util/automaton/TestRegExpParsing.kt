9576c6c6cabdd4c780acf8b9ccfecb0faa3dc201
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-06-17 16:26:26 +0900

    Port additional RegExp parsing tests

--- core/src/commonTest/kotlin/org/gnit/lucenekmp/util/automaton/TestRegExpParsing.kt
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/util/automaton/TestRegExpParsing.kt
@@ -32,6 +32,49 @@ class TestRegExpParsing {
         assertSameLanguage(expected, actual)
     }
 
+    @Test
+    fun testChar() {
+        val re = RegExp("c")
+        assertEquals("\\c", re.toString())
+        assertEquals("REGEXP_CHAR char=c\n", re.toStringTree())
+
+        val actual = re.toAutomaton()!!
+        AutomatonTestUtil.assertMinimalDFA(actual)
+
+        val expected = Automata.makeChar('c'.code)
+        assertSameLanguage(expected, actual)
+    }
+
+    @Test
+    fun testCaseInsensitiveChar() {
+        val re = RegExp("c", RegExp.NONE, RegExp.ASCII_CASE_INSENSITIVE)
+        assertEquals("\\c", re.toString())
+        assertEquals("REGEXP_CHAR char=c\n", re.toStringTree())
+
+        val actual = re.toAutomaton()!!
+        AutomatonTestUtil.assertMinimalDFA(actual)
+
+        val expected = Automata.makeCharSet(intArrayOf('c'.code, 'C'.code))
+        assertSameLanguage(expected, actual)
+    }
+
+    @Test
+    fun testCaseInsensitiveClassChar() {
+        val re = RegExp("[c]", RegExp.NONE, RegExp.ASCII_CASE_INSENSITIVE)
+        assertEquals(
+            "REGEXP_CHAR_CLASS starts=[U+0043 U+0063] ends=[U+0043 U+0063]\n",
+            re.toStringTree()
+        )
+        AutomatonTestUtil.assertMinimalDFA(re.toAutomaton()!!)
+    }
+
+    @Test
+    fun testCaseInsensitiveClassRange() {
+        val re = RegExp("[c-d]", RegExp.NONE, RegExp.ASCII_CASE_INSENSITIVE)
+        assertEquals("REGEXP_CHAR_RANGE from=c to=d\n", re.toStringTree())
+        AutomatonTestUtil.assertMinimalDFA(re.toAutomaton()!!)
+    }
+
     private fun assertSameLanguage(expected: Automaton, actual: Automaton) {
         val detExpected = Operations.determinize(expected, Operations.DEFAULT_DETERMINIZE_WORK_LIMIT)
         val detActual = Operations.determinize(actual, Operations.DEFAULT_DETERMINIZE_WORK_LIMIT)
