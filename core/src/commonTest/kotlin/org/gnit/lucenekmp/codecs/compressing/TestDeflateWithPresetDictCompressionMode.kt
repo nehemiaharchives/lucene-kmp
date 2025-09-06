7f012ac7d38766b8d313daec4e692b9e95b53867
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-09-06 13:18:45 +0900

    TestDeflateWithPresetDictCompressionMode passes all tests

--- core/src/commonTest/kotlin/org/gnit/lucenekmp/codecs/compressing/TestDeflateWithPresetDictCompressionMode.kt
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/codecs/compressing/TestDeflateWithPresetDictCompressionMode.kt
@@ -2,7 +2,6 @@ package org.gnit.lucenekmp.codecs.compressing
 
 import org.gnit.lucenekmp.codecs.lucene90.DeflateWithPresetDictCompressionMode
 import kotlin.test.BeforeTest
-import kotlin.test.Ignore
 import kotlin.test.Test
 
 class TestDeflateWithPresetDictCompressionMode : AbstractTestCompressionMode() {
@@ -14,16 +13,12 @@ class TestDeflateWithPresetDictCompressionMode : AbstractTestCompressionMode() {
     @Test
     override fun testDecompress() = super.testDecompress()
 
-    @Ignore
     @Test
     override fun testConstant() = super.testConstant()
 
-    @Ignore
     @Test
     override fun testExtremelyLargeInput() = super.testExtremelyLargeInput()
 
-    @Ignore
     @Test
     override fun testPartialDecompress() = super.testPartialDecompress()
 }
-
