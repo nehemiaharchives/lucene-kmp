cfa780de3433b9283a4624086dc351c7b3309c53
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-02-20 19:32:47 +0900

    TestIntDoubleHashMap and dependencies pass

---
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/internal/hppc/DoubleCursor.kt
@@ -0,0 +1,21 @@
+package org.gnit.lucenekmp.internal.hppc
+
+/**
+ * Forked from HPPC, holding int index and double value.
+ *
+ * @lucene.internal
+ */
+class DoubleCursor {
+    /**
+     * The current value's index in the container this cursor belongs to. The meaning of this index is
+     * defined by the container (usually it will be an index in the underlying storage buffer).
+     */
+    var index: Int = 0
+
+    /** The current value.  */
+    var value: Double = 0.0
+
+    override fun toString(): String {
+        return "[cursor, index: $index, value: $value]"
+    }
+}
