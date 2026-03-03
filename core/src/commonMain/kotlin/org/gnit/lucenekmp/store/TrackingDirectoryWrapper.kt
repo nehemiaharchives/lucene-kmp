07443fd06449e365c80f2af7d5d81afbf251947c
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-03-03 12:47:04 +0900

    TestTrackingDirectoryWrapper passes

--- core/src/commonMain/kotlin/org/gnit/lucenekmp/store/TrackingDirectoryWrapper.kt
+++ core/src/commonMain/kotlin/org/gnit/lucenekmp/store/TrackingDirectoryWrapper.kt
@@ -1,54 +1,87 @@
 package org.gnit.lucenekmp.store
 
+import org.gnit.lucenekmp.jdkport.ReentrantLock
 import okio.IOException
 
 
 /** A delegating Directory that records which files were written to and deleted.  */
 class TrackingDirectoryWrapper(`in`: Directory) : FilterDirectory(`in`) {
     private val createdFileNames: MutableSet<String> = mutableSetOf()
+    private val createdFileNamesLock = ReentrantLock()
 
     @Throws(IOException::class)
     override fun deleteFile(name: String) {
         `in`.deleteFile(name)
-        createdFileNames.remove(name)
+        try {
+            createdFileNamesLock.lock()
+            createdFileNames.remove(name)
+        } finally {
+            createdFileNamesLock.unlock()
+        }
     }
 
     @Throws(IOException::class)
     override fun createOutput(name: String, context: IOContext): IndexOutput {
         val output: IndexOutput = `in`.createOutput(name, context)
-        createdFileNames.add(name)
+        try {
+            createdFileNamesLock.lock()
+            createdFileNames.add(name)
+        } finally {
+            createdFileNamesLock.unlock()
+        }
         return output
     }
 
     @Throws(IOException::class)
     override fun createTempOutput(prefix: String, suffix: String, context: IOContext): IndexOutput {
         val tempOutput: IndexOutput = `in`.createTempOutput(prefix, suffix, context)
-        createdFileNames.add(tempOutput.name!!)
+        try {
+            createdFileNamesLock.lock()
+            createdFileNames.add(tempOutput.name!!)
+        } finally {
+            createdFileNamesLock.unlock()
+        }
         return tempOutput
     }
 
     @Throws(IOException::class)
     override fun copyFrom(from: Directory, src: String, dest: String, context: IOContext) {
         `in`.copyFrom(from, src, dest, context)
-        createdFileNames.add(dest)
+        try {
+            createdFileNamesLock.lock()
+            createdFileNames.add(dest)
+        } finally {
+            createdFileNamesLock.unlock()
+        }
     }
 
     @Throws(IOException::class)
     override fun rename(source: String, dest: String) {
         `in`.rename(source, dest)
-
-        // in kotlin common, synchronized is not available
-        // synchronized(createdFileNames) {
+        try {
+            createdFileNamesLock.lock()
             createdFileNames.add(dest)
             createdFileNames.remove(source)
-        //}
+        } finally {
+            createdFileNamesLock.unlock()
+        }
     }
 
     val createdFiles: MutableSet<String>
         /** NOTE: returns a copy of the created files.  */
-        get() = HashSet(createdFileNames)
+        get() = try {
+            createdFileNamesLock.lock()
+            HashSet(createdFileNames)
+        } finally {
+            createdFileNamesLock.unlock()
+        }
 
     fun clearCreatedFiles() {
-        createdFileNames.clear()
+        try {
+            createdFileNamesLock.lock()
+            createdFileNames.clear()
+        } finally {
+            createdFileNamesLock.unlock()
+        }
     }
 }
