69f1f5951c9582b3a2f7b9a0cd8178843f989bea
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-03-04 12:11:36 +0900

    Test2BSortedDocValuesFixedSorted passes

---
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/index/Test2BSortedDocValuesFixedSorted.kt
@@ -0,0 +1,83 @@
+package org.gnit.lucenekmp.index
+
+import org.gnit.lucenekmp.document.Document
+import org.gnit.lucenekmp.document.SortedDocValuesField
+import org.gnit.lucenekmp.tests.analysis.MockAnalyzer
+import org.gnit.lucenekmp.tests.index.Test2BConstants
+import org.gnit.lucenekmp.tests.store.BaseDirectoryWrapper
+import org.gnit.lucenekmp.tests.store.MockDirectoryWrapper
+import org.gnit.lucenekmp.tests.util.LuceneTestCase
+import org.gnit.lucenekmp.tests.util.TestUtil
+import org.gnit.lucenekmp.util.BytesRef
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+//@SuppressCodecs("SimpleText", "Direct")
+//@TimeoutSuite(millis = 80 * TimeUnits.HOUR) // effectively no limit
+// The six hour time was achieved on a Linux 3.13 system with these specs:
+// 3-core AMD at 2.5Ghz, 12 GB RAM, 5GB test heap, 2 test JVMs, 2TB SATA.
+//@Monster("Takes ~ 6 hours if the heap is 5gb")
+//@SuppressSysoutChecks(bugUrl = "Stuff gets printed")
+class Test2BSortedDocValuesFixedSorted : LuceneTestCase() {
+
+    // indexes Integer.MAX_VALUE docs with a fixed binary field
+    @Test
+    @Throws(Exception::class)
+    fun testFixedSorted() {
+        val dir: BaseDirectoryWrapper = newFSDirectory(createTempDir("2BFixedSorted"))
+        if (dir is MockDirectoryWrapper) {
+            dir.setThrottling(MockDirectoryWrapper.Throttling.NEVER)
+        }
+
+        val w = IndexWriter(
+            dir,
+            IndexWriterConfig(MockAnalyzer(random()))
+                .setMaxBufferedDocs(IndexWriterConfig.DISABLE_AUTO_FLUSH)
+                .setRAMBufferSizeMB(256.0)
+                .setMergeScheduler(ConcurrentMergeScheduler())
+                .setMergePolicy(newLogMergePolicy(false, 10))
+                .setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+                .setCodec(TestUtil.getDefaultCodec())
+        )
+
+        val doc = Document()
+        val bytes = ByteArray(2)
+        val data = BytesRef(bytes)
+        val dvField = SortedDocValuesField("dv", data)
+        doc.add(dvField)
+
+        for (i in 0..<Test2BConstants.MAX_DOCS) { // TODO reduced maxDocs = IndexWriter.MAX_DOCS to Test2BConstants.MAX_DOCS for dev speed
+            bytes[0] = (i shr 8).toByte()
+            bytes[1] = i.toByte()
+            w.addDocument(doc)
+            if (i % 100000 == 0) {
+                println("indexed: $i")
+            }
+        }
+
+        w.forceMerge(1)
+        w.close()
+
+        println("verifying...")
+
+        val r = DirectoryReader.open(dir)
+        var expectedValue = 0
+        for (context in r.leaves()) {
+            val reader = context.reader()
+            val dv = DocValues.getSorted(reader, "dv")
+            for (i in 0..<reader.maxDoc()) {
+                assertEquals(i, dv.nextDoc())
+                bytes[0] = (expectedValue shr 8).toByte()
+                bytes[1] = expectedValue.toByte()
+                val term = dv.lookupOrd(dv.ordValue())
+                assertEquals(data, term)
+                expectedValue++
+            }
+        }
+
+        r.close()
+        dir.close()
+    }
+
+    // TODO: variable
+}
