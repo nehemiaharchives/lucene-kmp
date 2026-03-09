1e24059b78440531eaaf1ce038fae5e96a6d68d9
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-03-09 18:58:42 +0900

    TestDuelingCodecsAtNight ignored

---
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/index/TestDuelingCodecsAtNight.kt
@@ -0,0 +1,12 @@
+package org.gnit.lucenekmp.index
+
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+class TestDuelingCodecsAtNight {
+
+    @Ignore // TODO ignored because it takes long time
+    @Test
+    fun testBigEquals(){}
+
+}
