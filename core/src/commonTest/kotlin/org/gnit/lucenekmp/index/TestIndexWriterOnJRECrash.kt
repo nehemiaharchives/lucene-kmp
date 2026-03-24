379c0ebf5e08157d9d3b079f34cab13f4376451c
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-03-24 12:08:29 +0900

    Ignore TestIndexWriterOnJRECrash

---
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/index/TestIndexWriterOnJRECrash.kt
@@ -0,0 +1,13 @@
+package org.gnit.lucenekmp.index
+
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+// TODO Ignore for now because KMP includes Kotlin/Native which is not based on JRE
+class TestIndexWriterOnJRECrash {
+
+    @Ignore
+    @Test
+    fun testNRTThreads(){}
+
+}
