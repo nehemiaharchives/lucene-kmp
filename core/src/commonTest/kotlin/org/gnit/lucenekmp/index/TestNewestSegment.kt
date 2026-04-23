2bedb14f65afd9db4174e32d75fdb48081eac663
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-04-23 18:45:35 +0900

    TestNewestSegment passes

---
+++ core/src/commonTest/kotlin/org/gnit/lucenekmp/index/TestNewestSegment.kt
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gnit.lucenekmp.index
+
+import org.gnit.lucenekmp.store.Directory
+import org.gnit.lucenekmp.tests.analysis.MockAnalyzer
+import org.gnit.lucenekmp.tests.util.LuceneTestCase
+import kotlin.test.Test
+import kotlin.test.assertNull
+
+class TestNewestSegment : LuceneTestCase() {
+    @Test
+    @Throws(Exception::class)
+    fun testNewestSegment() {
+        val directory: Directory = newDirectory()
+        val writer =
+            IndexWriter(directory, newIndexWriterConfig(MockAnalyzer(random())))
+        assertNull(writer.newestSegment())
+        writer.close()
+        directory.close()
+    }
+}
